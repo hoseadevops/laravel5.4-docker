d2c542ade2ed95d901747286a69b171121f89abe
hexing <he.xing@9douyu.com>
2017-06-08 11:53:14 +0800

    readme

--- docker.sh
+++ docker.sh
@@ -3,18 +3,20 @@
 set -e
 
 project_path=$(cd $(dirname $0); pwd -P)
+
 project_docker_path="$project_path/docker"
 
-developer_name=$('whoami')
-app_basic_name="laravel"
-app="$developer_name-$app_basic_name"
-
-#----------------------
-# dev test pre prod
-#----------------------
-env=dev
-
 source $project_docker_path/bash.sh
+
+developer_name=$('whoami');
+
+app_basic_name=$(read_kv_config .env APP_NAME);
+#----------------------
+# laravel: local testing production pre[后加 预生产]
+#----------------------
+app_env=$(read_kv_config .env APP_ENV);
+
+app="$developer_name-$app_basic_name"
 
 source $project_docker_path/busybox/container.sh
 
@@ -30,7 +32,6 @@ source $project_docker_path/nginx/container.sh
 
 help()
 {
-
 cat <<EOF
     Usage: sh docker.sh [options]
 
@@ -71,12 +72,13 @@ cat <<EOF
 
         updateHost www.baidu.com 127.0.0.1
 
+        read_kv_config .env DB_PORT
+
         help  show this message
 EOF
-
 }
 
-ALL_COMMANDS="updateHost init clean clean_all new_egg download_code pull_code build_code_config run_nginx_fpm rm_nginx restart_nginx run_mysql rm_mysql restart_mysql to_mysql delete_mysql build_php run_php to_php rm_php _run_cmd_php_container run_redis to_redis rm_redis restart_redis rm_busybox run_busybox run_syslogng rm_syslogng restart_syslogng"
+ALL_COMMANDS="read_kv_config updateHost init clean clean_all new_egg download_code pull_code build_code_config run_nginx_fpm rm_nginx restart_nginx run_mysql rm_mysql restart_mysql to_mysql delete_mysql build_php run_php to_php rm_php _run_cmd_php_container run_redis to_redis rm_redis restart_redis rm_busybox run_busybox run_syslogng rm_syslogng restart_syslogng"
 list_contains ALL_COMMANDS "$action" || action=help
 $action "$@"
 
